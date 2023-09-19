80192caf459a2e95374e3664022915f2fc74b2ca
stevegwh <sgwheeler@posteo.net>
2023-09-19 15:14:51 +1200

    HelloTriangle

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <array>
+
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 FragColor;\n"
+                                   "\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   "}\0";
 
 int main() {
     GLFWwindow* window;
@@ -20,17 +39,88 @@ int main() {
 
     glfwMakeContextCurrent(window);
 
-    float vertices[] = {
+    std::array<float, 9> vertices = {
             0.0, 0.5, 0.0,
             -0.5, -0.5, 0.0,
             0.5, -0.5, 0.0
     };
+    
+    std::array<glm::vec3, 3> vertices2 = {
+        glm::vec3(vertices[0], vertices[1], vertices[2]), 
+        glm::vec3(vertices[3], vertices[4], vertices[5]),
+        glm::vec3(vertices[6], vertices[7], vertices[8])
+    };
+    
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
 
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size(), vertices.data(), GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    float angle = 0.1f;
     while (!glfwWindowShouldClose(window))
     {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glEnableClientState(GL_VERTEX_ARRAY);
-        glVertexPointer(3, GL_FLOAT, 0, vertices);
+//        for (auto & i : vertices2) 
+//        {
+//            glm::mat4 trans = glm::mat4(1.0f);
+//            trans = glm::rotate(trans, glm::radians(angle), { 1, 0, 0 });
+//            glm::vec4 result = trans * glm::vec4(i, 1.0f);
+//            i = glm::vec3 (result.x, result.y ,result.z);
+//        }
+//        vertices[0] = vertices2[0].x;
+//        vertices[1] = vertices2[0].y;
+//        vertices[2] = vertices2[0].z;
+//        vertices[3] = vertices2[1].x;
+//        vertices[4] = vertices2[1].y;
+//        vertices[5] = vertices2[1].z;
+//        vertices[6] = vertices2[2].x;
+//        vertices[7] = vertices2[2].y;
+//        vertices[8] = vertices2[2].z;
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
 
